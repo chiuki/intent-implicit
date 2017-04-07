508614eafe1ca4c9bf8b450ba594f3796ec44480
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2017-04-07 15:31:44 -0600

    Add MainActivity

---
+++ app/src/main/java/com/sqisland/intent/implicit/MainActivity.java
@@ -0,0 +1,37 @@
+package com.sqisland.intent.implicit;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void link(View view) {
+  }
+
+  public void mapLink(View view) {
+  }
+
+  public void mapGeo(View view) {
+  }
+
+  public void dial(View view) {
+  }
+
+  public void gallery(View view) {
+  }
+
+  public void sendText(View view) {
+  }
+
+  public void sendImage(View view) {
+  }
+
+  public void sendEmail(View view) {
+  }
+}
